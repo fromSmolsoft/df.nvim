ab614835be140bae03e95aef2ddaed0e1c71083d
SmolSoft <smolsoft@email.cz>
2024-08-23 19:54:33 +0200

    auto-session, whichkeys

--- init.lua
+++ init.lua
@@ -1,17 +1,22 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+-- Auto instal lazy.nvim plugin manager
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-
+-- load custom config(s)
 require("keymaps")
 require("options")
+
+-- load lazy.nvim and pass in plugins dir to autoload all plugins
 require("lazy").setup("plugins")
---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- referto the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
