09a689bf34dc539a4d73fb831d8306de7ab67577
smolsoft <smolsoft@email.cz>
2024-11-01 12:57:33 +0100

    keymaps edited: close buffers, descriptions

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,3 @@
-
 local vkey = vim.keymap
 
 -- Leader key
@@ -7,7 +6,10 @@ vim.g.maplocalleader = ' '
 
 -- navigate between panes by ctrl + [hjkl]
 vkey.set('n', '<leader>h', ':nohlsearch<CR>')
-vkey.set('n', '<c-h>', ':wincmd h<CR>')
-vkey.set('n', '<c-j>', ':wincmd j<CR>')
-vkey.set('n', '<c-k>', ':wincmd k<CR>')
-vkey.set('n', '<c-l>', ':wincmd l<CR>')
+vkey.set('n', '<c-h>', ':wincmd h<CR>', { desc = "Go to left pane" })
+vkey.set('n', '<c-j>', ':wincmd j<CR>', { desc = "Go to bottom pane" })
+vkey.set('n', '<c-k>', ':wincmd k<CR>', { desc = "Go to top pane" })
+vkey.set('n', '<c-l>', ':wincmd l<CR>', { desc = "Go to right pane" })
+
+vkey.set('n', '<leader>bd', ':%bd|e#<CR>', { desc = "Close saved buffers" })
+vkey.set('n', '<leader>*', '*#:%s///gc<left><left><left>', { desc = "Search & replace" })
