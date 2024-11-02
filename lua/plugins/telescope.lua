b0b70244a8c44ed0ca6537ded7a6a9bd3d49c805
smolsoft <smolsoft@email.cz>
2024-11-02 16:22:43 +0100

    telescope keybinings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,10 +15,19 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
+    -- Find
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {desc ="Find files"})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc ="Find files"})
+      vim.keymap.set("n", "<leader>fc", builtin.commands, {desc ="Find cmd"})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {desc ="Find buffers"})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find in files"})
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Find oldFiles"})
+
+    -- Git
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits"})
+      vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git diff"})
+      vim.keymap.set("n", "<leader>gb", builtin.git_stash, { desc = "Git stash"})
       require("telescope").load_extension("ui-select")
     end,
   },
