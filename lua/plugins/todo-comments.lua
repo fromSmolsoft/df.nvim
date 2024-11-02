0896be56787a69ea8760c80505c3634d9767771f
smolsoft <smolsoft@email.cz>
2024-11-02 15:16:17 +0100

    todo-comments: keybindings

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -5,5 +5,9 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-    }
+    },
+    vim.keymap.set("n", "<leader>tt", ":TodoTelescope<CR>", { desc = "TODO Telescope" }),
+    vim.keymap.set("n", "<leader>tl", ":TodoLocList<CR>", { desc = "TODO List" }),
+    -- vim.keymap.set("n", "<leader>tf", ":TodoQuickFix<CR>", { desc = "TODO QuickFix" }),
+
 }
