b04f87fe22ac1206b7f8823c9c8dd77dbad9555e
smolsoft <smolsoft@email.cz>
2024-12-02 09:49:33 +0100

    nvim-jdtls: cleaning
    - modified:   lua/plugins/nvim-jdtls.lua

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -3,10 +3,7 @@ return {
     'mfussenegger/nvim-jdtls',
     dependencies = 'hrsh7th/cmp-nvim-lsp',
     -- https://github.com/mfussenegger/nvim-jdtls
-    -- doesn't seem to do more than Mason jdtls
     enabled = true,
-    --  It loads jdtls upon opening first java file (buffer)  FIX: when opening another file in same session, jdtls doesn't attach to newly opened buffer, possible solution => create autocomand based on filetype java
-    -- ft = "java",
 
     -- setup options
     opts = function()
@@ -28,11 +25,10 @@ return {
 
     -- setup nvim-jdtls
     config = function(_, opts)
-        -- autocomand to start_or_attach this only for java 
+        -- vim api auto-command to start_or_attach this only for java
         vim.api.nvim_create_autocmd("FileType", {
             pattern = "java",
             callback = function()
-
                 print("Starting JDTLS...")
                 local success, result = pcall(require('jdtls').start_or_attach, opts)
                 if success then
@@ -41,135 +37,6 @@ return {
                     print("Error starting JDTLS: " .. tostring(result))
                 end
             end
-        }
-        )
+        })
     end
 }
--- alternative config source: https://www.reddit.com/r/neovim/comments/11k3zuv/comment/jb5q1bm/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
---return
--- {
---     {
---         "neovim/nvim-lspconfig",
---         dependencies = { "mfussenegger/nvim-jdtls" },
---         opts = {
---             setup = {
---                 jdtls = function(_, opts)
---                     vim.api.nvim_create_autocmd("FileType", {
---                         pattern = "java",
---                         callback = function()
---                             require("lazyvim.util").on_attach(function(_, buffer)
---                                 vim.keymap.set(
---                                     "n",
---                                     "<leader>di",
---                                     "<Cmd>lua require'jdtls'.organize_imports()<CR>",
---                                     { buffer = buffer, desc = "Organize Imports" }
---                                 )
---                                 vim.keymap.set(
---                                     "n",
---                                     "<leader>dt",
---                                     "<Cmd>lua require'jdtls'.test_class()<CR>",
---                                     { buffer = buffer, desc = "Test Class" }
---                                 )
---                                 vim.keymap.set(
---                                     "n",
---                                     "<leader>dn",
---                                     "<Cmd>lua require'jdtls'.test_nearest_method()<CR>",
---                                     { buffer = buffer, desc = "Test Nearest Method" }
---                                 )
---                                 vim.keymap.set(
---                                     "v",
---                                     "<leader>de",
---                                     "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
---                                     { buffer = buffer, desc = "Extract Variable" }
---                                 )
---                                 vim.keymap.set(
---                                     "n",
---                                     "<leader>de",
---                                     "<Cmd>lua require('jdtls').extract_variable()<CR>",
---                                     { buffer = buffer, desc = "Extract Variable" }
---                                 )
---                                 vim.keymap.set(
---                                     "v",
---                                     "<leader>dm",
---                                     "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
---                                     { buffer = buffer, desc = "Extract Method" }
---                                 )
---                                 vim.keymap.set(
---                                     "n",
---                                     "<leader>cf",
---                                     "<cmd>lua vim.lsp.buf.formatting()<CR>",
---                                     { buffer = buffer, desc = "Format" }
---                                 )
---                             end)
---
---                             local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
---                             -- vim.lsp.set_log_level('DEBUG')
---                             local workspace_dir = "$HOME/.workspace/" ..
---                             project_name                                 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
---                             local config = {
---                                 -- The command that starts the language server
---                                 -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
---                                 cmd = {
---
---                                     "java", -- or '/path/to/java17_or_newer/bin/java'
---                                     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
---
---                                     "-javaagent:$HOME/.local/share/java/lombok.jar",
---                                     -- '-Xbootclasspath/a:$HOME/.local/share/java/lombok.jar',
---                                     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
---                                     "-Dosgi.bundles.defaultStartLevel=4",
---                                     "-Declipse.product=org.eclipse.jdt.ls.core.product",
---                                     "-Dlog.protocol=true",
---                                     "-Dlog.level=ALL",
---                                     -- '-noverify',
---                                     "-Xms1g",
---                                     "--add-modules=ALL-SYSTEM",
---                                     "--add-opens",
---                                     "java.base/java.util=ALL-UNNAMED",
---                                     "--add-opens",
---                                     "java.base/java.lang=ALL-UNNAMED",
---                                     "-jar",
---                                     vim.fn.glob("/usr/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
---                                     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
---                                     -- Must point to the                                                     Change this to
---                                     -- eclipse.jdt.ls installation                                           the actual version
---
---                                     "-configuration",
---                                     "/usr/share/java/jdtls/config_linux",
---                                     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
---                                     -- Must point to the                      Change to one of `linux`, `win` or `mac`
---                                     -- eclipse.jdt.ls installation            Depending on your system.
---
---                                     -- See `data directory configuration` section in the README
---                                     "-data",
---                                     workspace_dir,
---                                 },
---
---                                 -- This is the default if not provided, you can remove it. Or adjust as needed.
---                                 -- One dedicated LSP server & client will be started per unique root_dir
---                                 root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
---
---                                 -- Here you can configure eclipse.jdt.ls specific settings
---                                 -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
---                                 -- for a list of options
---                                 settings = {
---                                     java = {},
---                                 },
---                                 handlers = {
---                                     ["language/status"] = function(_, result)
---                                         -- print(result)
---                                     end,
---                                     ["$/progress"] = function(_, result, ctx)
---                                         -- disable progress updates.
---                                     end,
---                                 },
---                             }
---                             require("jdtls").start_or_attach(config)
---                         end,
---                     })
---                     return true
---                 end,
---             },
---         },
---     },
--- }
