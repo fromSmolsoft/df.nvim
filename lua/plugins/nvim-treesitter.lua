b2e322558db63ab52e51dd09dac81946f997a2bf
smolsoft <smolsoft@email.cz>
2024-12-03 10:10:37 +0100

    Treesitter: ensure_installed -> added languages
    - modified:   lua/plugins/nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         opts = {
-            ensure_installed = { "lua", "java", "html", "vim", "vimdoc", "bash", "javascript", "python", "xml", "markdown" },
+            ensure_installed = { "lua", "java", "html", "vim", "vimdoc", "bash", "javascript", "python", "xml", "markdown", "groovy",  "gitignore", "gitcommit"},
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
