c895b01badfac44244f72b6346e6bff3cb5502b6
smolsoft <smolsoft@email.cz>
2024-12-05 12:36:07 +0100

    Telescope: `oldfile` key mapping changed
    - <leader><leader> now does ":Telescope oldfile
    cwd_only=true"
    - used to do `builtin.oldfiles` but that got all files
    
    - modified:   lua/plugins/telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -31,7 +31,9 @@ return {
             vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Find cmd" })
             vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find in files" })
-            vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Find oldFiles" })
+            -- TODO: use builtin.oldfiles with argument cwd_only=true instead of command
+            -- vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Find oldFiles" })
+            vim.keymap.set("n", "<leader><leader>", ":Telescope oldfiles cwd_only=true<CR>", { desc = "Find oldFiles" })
 
             -- Git
             vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits" })
