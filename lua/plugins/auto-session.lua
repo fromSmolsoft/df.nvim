91726e65a35375faed7cc7d96625dee8a6e186a7
smolsoft <smolsoft@email.cz>
2024-12-07 14:20:50 +0100

    auto-session: removed plugin setup
    - Lazy plugin manager does plugin setup automatically
    
    - modified:   lua/plugins/auto-session.lua

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -12,22 +12,20 @@ return {
         { '<leader>ts', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
     },
 
-    config = function()
-        require("auto-session").setup {
-            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-            -- ⚠️ This will only work if Telescope.nvim is installed
-            -- The following are already the default values, no need to provide them if these are already the settings you want.
-            session_lens = {
-                -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
-                load_on_setup = true,
-                theme_conf = { border = true },
-                previewer = false,
-                mappings = {
-                    -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
-                    delete_session = { "i", "<C-D>" },
-                    alternate_session = { "i", "<C-S>" },
-                },
-            }
+    opts = {
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+        -- ⚠️ This will only work if Telescope.nvim is installed
+        -- The following are already the default values, no need to provide them if these are already the settings you want.
+        session_lens = {
+            -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
+            load_on_setup = true,
+            theme_conf = { border = true },
+            previewer = false,
+            mappings = {
+                -- Mode can be a string or a table, e.g. {"i", "n"} for both insert and normal mode
+                delete_session = { "i", "<C-D>" },
+                alternate_session = { "i", "<C-S>" },
+            },
         }
-    end,
+    }
 }
