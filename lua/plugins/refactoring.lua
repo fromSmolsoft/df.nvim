315f4ba26bddb93b54cd88d47848173ea5f94f85
smolsoft <smolsoft@email.cz>
2024-12-10 20:44:09 +0100

    Refactoring: plugin for basic refactoring (extract,inline,...) - modified:   lua/plugins/refactoring.lua

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,5 +1,23 @@
 return {
     {
+        "ThePrimeagen/refactoring.nvim",
+        enabled = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        -- lazy = false,
+        opts = {
+            vim.keymap.set("x", "<leader>re", ":Refactor extract "),
+            vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file "),
+            vim.keymap.set("x", "<leader>rv", ":Refactor extract_var "),
+            vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var"),
+            vim.keymap.set("n", "<leader>rI", ":Refactor inline_func"),
+            vim.keymap.set("n", "<leader>rb", ":Refactor extract_block"),
+            vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file"),
+        },
+    },
+    {
         -- python
         "alexpasmantier/pymple.nvim", -- https://github.com/alexpasmantier/pymple.nvim
         -- Pymple adds missing common Python IDE features for Neovim when dealing with imports
