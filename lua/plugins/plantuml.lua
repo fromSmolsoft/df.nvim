ecf16c3169c998d9f83c622b178b3eae9897fa19
smolsoft <smolsoft@email.cz>
2024-12-14 10:21:36 +0100

    plantuml,Soil,which-key group `UML` added
    - modified:   lua/plugins/plantuml.lua

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -6,8 +6,6 @@ return
         --     vim.api.nvim_create_autocmd("FileType", {
         --         pattern = "plantuml",
         --         callback = function()
-        -- run plant uml in bash with currently open file , no need to save it first
-        vim.keymap.set("n", "<leader>us", ":!plantuml % -svg<CR>", { desc = "Puml->svg" }),
         vim.keymap.set("n", "<leader>up", ":!plantuml % -png<CR>", { desc = "Puml->png" }),
         vim.keymap.set("n", "<leader>ud", ":!plantuml % -dpng<CR>", { desc = "Puml->dpng" }),
         --         end
@@ -21,27 +19,29 @@ return
         -- branch = "unfreeze",
         ft = "plantuml",
         -- Optional for puml syntax highlighting:
-        dependencies = { 'javiorfo/nvim-nyctophilia' },
+        dependencies = { 'javiorfo/nvim-nyctophilia', "folke/which-key.nvim" },
         -- lazy = true,
-        opts = {
+        opts = function()
             -- If you want to use Plant UML jar version instead of the install version
             -- puml_jar = "/path/to/plantuml.jar",
 
             -- If you want to customize the image showed when running this plugin
             image = {
-                darkmode = false, -- Enable or disable darkmode
-                format = "png",   -- Choose between png or svg (svg has white background)
+                    darkmode = false, -- Enable or disable darkmode
+                    format = "png",   -- Choose between png or svg (svg has white background)
 
-                -- This is a default implementation of using nsxiv to open the resultant image
-                -- Edit the string to use your preferred app to open the image (as if it were a command line)
-                -- Some examples:
-                -- return "feh " .. img
-                -- return "xdg-open " .. img
-                execute_to_open = function(img)
-                    return "nsxiv -b " .. img
-                end,
+                    -- This is a default implementation of using nsxiv to open the resultant image
+                    -- Edit the string to use your preferred app to open the image (as if it were a command line)
+                    -- Some examples:
+                    -- return "feh " .. img
+                    -- return "xdg-open " .. img
+                    execute_to_open = function(img)
+                        return "nsxiv -b " .. img
+                    end,
+                },
+                require("which-key").add({ "<leader>u", group = "UML" }),
                 vim.keymap.set("n", "<leader>ur", ":Soil", { desc = "Puml->img & open" })
-            }
-        },
+        end
+
     }
 }
