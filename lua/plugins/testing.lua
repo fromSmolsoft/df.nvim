a39181c8f08d080da74d437f8261f9526dfd0f7e
smolsoft <smolsoft@email.cz>
2024-12-16 09:14:40 +0100

    Keymaping, which-key.nvim groups
    - modified:   lua/plugins/refactoring.lua
    - modified:   lua/plugins/testing.lua
    - modified:   lua/plugins/trouble-nvim.lua
    - modified:   lua/plugins/whichkey.lua

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -8,10 +8,19 @@ return
         'rcasia/neotest-java',
     },
     config = function()
-        require 'neotest'.setup({
+        local neotest = require("neotest")
+
+        neotest.setup({
             adapters = {
                 require 'neotest-java',
             },
         })
+
+        -- keymaps
+        local wk = require("which-key")
+        if wk ~= nil then wk.add({ "<leader>t", group = "Testing" }) end
+        local test_file = function() neotest.run.run(vim.fn.expand("%")) end
+        vim.keymap.set({ "n", "v" }, "<leader>tn", neotest.run.run, { desc = "Run test nearest" })
+        vim.keymap.set({ "n", "v" }, "<leader>tf", test_file, { desc = "Run tests in file" })
     end
 }
