9d94f5f592c2c37d61baffe355c12e1c8023dce0
smolsoft <smolsoft@email.cz>
2025-01-14 10:33:05 +0100

    codeium: load on keys - codeium plugin now loads on key - Issues: plugin can't be disabled manually "on the go"
    - modified:   lua/plugins/codeium.lua

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,7 +1,21 @@
+local l_loaded = false;
+--- Helper function as a toggle plugin.
+local enable_codeium = function()
+    if l_loaded then
+        l_loaded = not l_loaded
+        vim.notify("Codeium enabled=" .. tostring(l_loaded))
+    else
+        -- FIXME: unload / disable plugin "on the go"
+        l_loaded = not l_loaded
+        vim.notify("TODO: not implemeneted" .. "Codeium enabled=" .. tostring(l_loaded))
+    end
+end;
 return {
+    --- Codeium ai assistant
     "Exafunction/codeium.nvim", -- https://github.com/Exafunction/codeium.nvim
+    keys = { { "<leader>ct", enable_codeium, desc = "Codeium: enabled" }, },
     event = "VeryLazy",
-    enabled = false,
+    -- cond = l_loaded,
     -- FIXME: inline hints  not working
     dependencies = {
         "nvim-lua/plenary.nvim",
