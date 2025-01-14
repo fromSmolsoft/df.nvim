267b93a3189e8b75f93d61254484fcd3157c854c
smolsoft <smolsoft@email.cz>
2025-01-14 08:30:10 +0100

    dadbod: removed plugin dependency.
    - Plugin `dependencies={}` is discouraged by lazy.nvim documentation.
    - `dependencies` complicate lazy loading by Lazy.nvim
    - Lazy nvim loads any plugin when required
    
    - modified:   lua/plugins/dadbod.lua

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,18 +1,27 @@
 return {
-    "kristijanhusak/vim-dadbod-ui",
-    -- ft = { "sql", "mysql", "plsql" },
-    dependencies = {
-        { "tpope/vim-dadbod",                     lazy = true },
-        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-    },
-    cmd = {
-        "DBUI",
-        "DBUIToggle",
-        "DBUIAddConnection",
-        "DBUIFindBuffer",
-    },
-    init = function()
-        -- Your DBUI configuration
-        vim.g.db_ui_use_nerd_fonts = 1
-    end,
+
+    { -- Interacting with databases. https://github.com/tpope/vim-dadbod
+        "tpope/vim-dadbod", lazy = true },
+    {
+        -- Auto-completion using dadbod database connection. https://github.com/kristijanhusak/vim-dadbod-completion.
+        -- Used as source in nvim-cmp.
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true
+    }, -- Optional
+    {
+        -- UI for dadbod.  https://github.com/kristijanhusak/vim-dadbod-ui
+        "kristijanhusak/vim-dadbod-ui",
+        -- ft = { "sql", "mysql", "plsql" },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    }
 }
