90246de612b284178f616ae774c5c091b4d321d8
smolsoft <smolsoft@email.cz>
2025-02-03 08:08:01 +0100

    ref: extracting settings to variables, comments - modified:   lua/plugins/nvim-jdtls.lua

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -10,6 +10,7 @@ local function get_os()
     local os_name = vim.fn.has("macunix") == 1 and "mac" or
         vim.fn.has("win32") == 1 and "win" or
         "linux"
+    return os_name
 
     -- Debug information
     vim.notify(string.format("Detected OS: %s", os_name))
@@ -21,6 +22,7 @@ end
 
 return
 {
+    -- FIXME: jdtls exits with code 13
     -- https://github.com/mfussenegger/nvim-jdtls
     "mfussenegger/nvim-jdtls",
     cond = true, -- don't use together with "nvim-java/nvim-java" or "lsp-config"'s jdtls
@@ -48,30 +50,33 @@ return
         -- Jdtls features
         -- Capabilities
         -- java-debug and java-test extensions
+        local bundles = { vim.split(
+            vim.fn.glob(mason_path .. "packages/java-*/extension/server/*.jar", 1), '\n'), }
 
         end
+        local cmd = {
+            vim.fn.expand "$HOME/.local/share/nvim/mason/bin/jdtls",
+            -- "java",
+            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+            "-Dosgi.bundles.defaultStartLevel=4",
+            "-Declipse.product=org.eclipse.jdt.ls.core.product",
+            "-Dlog.protocol=true",
+            "-Dlog.level=ALL",
+            "-Xmx1G",
+            "--add-modules=ALL-SYSTEM",
+            "--add-opens", "java.base/java.util=ALL-UNNAMED",
+            "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+            "--jvm-arg=-javaagent:" .. lombok_path,
+            "-jar", launcher,
+            "-configuration", path_to_config,
+            "-data", workspace_path,
+        }
 
         local path_to_config = path_to_jdtls .. "/config_" .. os
         local lombok_path = path_to_jdtls .. "/lombok.jar"
         local launcher = vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar")
 
         return {
-            cmd = {
-                vim.fn.expand '$HOME/.local/share/nvim/mason/bin/jdtls',
-                '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-                '-Dosgi.bundles.defaultStartLevel=4',
-                '-Declipse.product=org.eclipse.jdt.ls.core.product',
-                '-Dlog.protocol=true',
-                '-Dlog.level=ALL',
-                '-Xmx1g',
-                '--add-modules=ALL-SYSTEM',
-                '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-                '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-                "--jvm-arg=-javaagent:" .. lombok_path,
-                "-jar", path_to_jar,
-                '-configuration', path_to_config,
-                '-data', workspace_dir,
-            },
 
             capabilities = require 'cmp_nvim_lsp'.default_capabilities(),
             bundles = vim.split(vim.fn.glob('$HOME/.local/share/nvim/mason/packages/java-*/extension/server/*.jar', 1),
@@ -127,6 +132,7 @@ return
                             "com",
                             "org",
                 -- Command + flags launching jdtls
+                cmd = cmd,
                         },
                     },
                     sources = {
@@ -166,14 +172,10 @@ return
                 }
             },
 
-            init_options = {
-                bundles = {
-                    vim.fn.glob(
-                        "/home/martin/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-                        1)
+                init_options = {
+                    bundles = bundles,
                 },
             },
-        }
 
             -- type :CheckJavaVersion to check java version
             vim.api.nvim_create_user_command(
