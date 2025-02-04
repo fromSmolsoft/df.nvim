4d622794d20845de644895754ea6146dc194520b
smolsoft <smolsoft@email.cz>
2025-02-04 17:49:18 +0100

    fix: fixed missing localoptions
    - reported issue - https://github.com/rmagatti/auto-session/issues/369
    
    - modified:   lua/plugins/auto-session.lua

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,32 +1,23 @@
 return {
+    -- https://github.com/rmagatti/auto-session?tab=readme-ov-file#%EF%B8%8F-configuration
     "rmagatti/auto-session",
     lazy = false,
-    dependencies = {
-        'nvim-telescope/telescope.nvim',
-        "folke/which-key.nvim",
-        -- FIXME: which-key.nvim groups is not added
-        optional = true,
-        opts = {
-            defaults = {
-                ["<leader>s"] = { name = "Session" },
-            },
-        },
-    },
+    -- dependencies = { 'nvim-telescope/telescope.nvim', },
     keys = {
-        -- require("which-key").add({ "<leader>s", group = "Sessions" }),
-
-        -- Will use Telescope if installed or a vim.ui.select picker otherwise
         { '<leader>sl', '<cmd>SessionSearch<CR>',         desc = 'Session search' },
-
         { '<leader>ss', '<cmd>SessionSave<CR>',           desc = 'Save session' },
         { '<leader>st', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
     },
     opts = {
+        -- fixes missing localoptions at https://github.com/rmagatti/auto-session/issues/369
+        (function()
+            vim.o.sessionoptions =
+            "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+        end)(),
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-        -- ⚠️ This will only work if Telescope.nvim is installed
-        -- The following are already the default values, no need to provide them if these are already the settings you want.
+
+        -- This will only work if Telescope.nvim is installed
         session_lens = {
-            -- If load_on_setup is false, make sure you use `:SessionSearch` to open the picker as it will initialize everything first
             load_on_setup = true,
             theme_conf = { border = true },
             previewer = false,
