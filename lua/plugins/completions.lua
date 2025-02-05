5a30372b3088ddd8f6eabf00ac623d4d63d06814
smolsoft <smolsoft@email.cz>
2025-02-05 09:00:02 +0100

    chore: formatting, cleaning
    - modified:   lua/plugins/completions.lua
    - modified:   lua/plugins/plantuml.lua

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,6 +10,7 @@ return {
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
         },
+        -- build = "make install_jsregexp",
     },
     {
         {
--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -9,7 +9,6 @@ return
         -- enabled = false,
     },
 
-
     --- plantuml previewers ---
     {
         "https://gitlab.com/itaranto/preview.nvim",
@@ -28,7 +27,7 @@ return
                         }
                     },
 
-                    --  FIXME: trows error bc plantuml doesn"t support ascii rendering for all diagram types
+                    --[[ INFO: plantuml_text - trows error - plantuml doesn't support ascii art for all diagram types]]
                     -- plantuml = { name = "plantuml_text", renderer = { type = "buffer", opts = { split_cmd = "split" } }, },
 
                     -- markdown = { name = "pandoc_wkhtmltopdf", renderer = { type = "command", opts = { cmd = { "zathura" } } }, },
@@ -42,6 +41,7 @@ return
             render_on_write = true,
         }
     },
+
     {
         -- TODO: activate shortcuts only for plantuml file type
         -- config = function()
@@ -54,15 +54,18 @@ return
         --     })
         -- end
     },
+
     {
         "javiorfo/nvim-soil",
         -- my own fork that fixes freezing
         -- "fromSmolsoft/nvim-soil",
         -- branch = "unfreeze",
+
         ft = "plantuml",
         -- Optional for puml syntax highlighting:
         -- dependencies = { "javiorfo/nvim-nyctophilia", "folke/which-key.nvim" },
         -- lazy = true,
+
         opts = function()
             -- If you want to use Plant UML jar version instead of the install version
             -- puml_jar = "/path/to/plantuml.jar",
@@ -84,6 +87,5 @@ return
                 require("which-key").add({ "<leader>u", group = "UML" }),
                 vim.keymap.set("n", "<leader>ur", ":Soil", { desc = "Puml->img & open" })
         end
-
     }
 }
