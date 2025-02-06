c92d39680febc52293054d3afc64c64da55ea7e2
smolsoft <smolsoft@email.cz>
2025-02-06 10:47:33 +0100

    chore: luadocs formatting

--- lua/utils/vutil.lua
+++ lua/utils/vutil.lua
@@ -1,12 +1,13 @@
---[[Collection of util specifically for Neovim.
+--[[
+Collection of util specifically for Neovim.
 [!] may depend on (Neo)vim   api
 ]]
 return {
 
     --[[
     Get current operation system as a string.
-    @returns string either "mac","win" or "linux"
-    --]]
+    @return string either "mac","win" or "linux"
+    ]]
     get_os = function()
         local os_name = vim.fn.has("macunix") == 1 and "mac" or
             vim.fn.has("win32") == 1 and "win" or
@@ -16,8 +17,8 @@ return {
 
     --[[
     Fetch user home folder.
-    @returns string path to user home directory
-    --]]
+    @return string path to user home directory
+    ]]
     get_userhome = function()
         return os.getenv("HOME")
     end,
@@ -26,9 +27,9 @@ return {
     --[[
     Verify whether file exists and can be open for reading.
     Opens file for reading temporarily.
-    @filename string
-    @returns true if file exists, false if file doesn't exists or can't be open for reading
-    --]]
+    @param filename - string, eg. "path/to/filename"
+    @return true if file exists, false if file doesn't exists or can't be open for reading
+    ]]
     is_file = function(filename)
         local file = io.open(filename, "r")
         if file ~= nill then
