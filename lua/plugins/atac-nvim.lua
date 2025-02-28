6580badc2e04601f3371419612cedd903b92aad8
smolsoft <smolsoft@email.cz>
2025-02-28 21:37:48 +0100

    fear: REST requests from Neovim
    atac-nvim  - plugin to use atac cli which is Postman like tool in
    terminal. Requires atac cli installed.

--- lua/plugins/atac-nvim.lua
+++ lua/plugins/atac-nvim.lua
@@ -2,10 +2,16 @@ return
 {
     -- REST requests without leaving neovim similar to postman, https://github.com/NachoNievaG/atac.nvim, requires atac >= 0.13.0
     "NachoNievaG/atac.nvim",
-    cond = false,
+    cond = vim.fn.has("nvim-0.9") == 1, -- enable only for nvim version >= 0.9
     dependencies = { "akinsho/toggleterm.nvim" },
-    opts = {
+    opts = function()
         -- TODO: store project specific files in `project_root/.tmp/atac/`
-        -- dir = vim.fn.resolve(cwd .. "/.atac"), -- By default, the dir will be set as /tmp/atac
-    },
+        local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+        local root_dir = vim.fs.dirname(vim.fs.find(root_markers, { upward = true })[1] or vim.fn.getcwd())
+        vim.notify("root_dir= \"" .. root_dir .. "\"")
+
+        return {
+            dir = vim.fn.resolve(root_dir .. "/.atac"), -- By default, the dir will be set as /tmp/atac
+        }
+    end
 }
