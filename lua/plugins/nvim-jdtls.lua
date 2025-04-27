f98e14001faee0e1695fa3e387035e34735e0583
smolsoft <smolsoft@email.cz>
2025-04-27 09:36:19 +0200

    feat(java): Java refactoring in Nvim-tree
    `simaxme/java.nvim` allows Nvim-tree file-rename properly refactor related code.

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -227,4 +227,25 @@ return { {
         -- "springboot-nvim" config
         return {}
     end
-}, }
+},
+
+    {
+        -- java refactoring extensions, https://github.com/simaxme/java.nvim,
+        'simaxme/java.nvim',
+        opts = {
+            rename = {
+                enable = true,  -- enable the functionality for renaming java files
+                nvimtree = true, -- enable nvimtree integration
+                write_and_close = false -- automatically write and close modified (previously unopened) files after refactoring a java file
+            },
+            snippets = {
+                enable = true -- enable the functionality for java snippets
+            },
+            root_markers = { -- markers for detecting the package path (the package path should start *after* the marker)
+                "main/java/",
+                "test/java/"
+            }
+        }
+    }
+
+}
