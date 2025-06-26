9c3811260c9690ec7b808547392e15ee439e7f19
smolsoft <smolsoft@email.cz>
2025-06-26 21:02:00 +0200

    chore(nvim-jdtls): formatting

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -197,7 +197,7 @@ return { {
             pattern = "java",
             group = java_augr,
             desc = TAG .. " start_or_attach",
-            -- TODO: Key-map to toggle JDTLS by nesting it inside this auto-command 
+            -- TODO: Key-map to toggle JDTLS by nesting it inside this auto-command
             callback = function()
                 vim.notify("[JDTLS]: start_or_attach", vim.log.levels.INFO)
                 local success, result = pcall(require("jdtls").start_or_attach, opts)
@@ -236,14 +236,14 @@ return { {
         'simaxme/java.nvim',
         opts = {
             rename = {
-                enable = true,  -- enable the functionality for renaming java files
-                nvimtree = true, -- enable nvimtree integration
+                enable = true,          -- enable the functionality for renaming java files
+                nvimtree = true,        -- enable nvimtree integration
                 write_and_close = false -- automatically write and close modified (previously unopened) files after refactoring a java file
             },
             snippets = {
                 enable = true -- enable the functionality for java snippets
             },
-            root_markers = { -- markers for detecting the package path (the package path should start *after* the marker)
+            root_markers = {  -- markers for detecting the package path (the package path should start *after* the marker)
                 "main/java/",
                 "test/java/"
             }
