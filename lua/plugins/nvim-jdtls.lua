8dd55932c554e4a08ef35dc2720eea982eb5c00c
smolsoft <smolsoft@email.cz>
2025-06-26 21:00:45 +0200

    feat(nvim-jdtls): enable native access

--- lua/plugins/nvim-jdtls.lua
+++ lua/plugins/nvim-jdtls.lua
@@ -99,6 +99,7 @@ return { {
             "--add-modules=ALL-SYSTEM",
             "--add-opens", "java.base/java.util=ALL-UNNAMED",
             "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+            "--enable-native-access=ALL-UNNAMED",
             "--jvm-arg=-javaagent:" .. lombok_path,
             "-jar", launcher,
             "-configuration", path_to_config,
